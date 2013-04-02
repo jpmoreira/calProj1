908e36533207a56922843ae1fa7f3ad117c44e06
jpmoreira <ei12002@fe.up.pt>
2013-04-02 12:13:47 +0100

    first Commit

---
+++ src/Coordinate.cpp
@@ -0,0 +1,18 @@
+/*
+ * Coordinate.cpp
+ *
+ *  Created on: Apr 2, 2013
+ *      Author: mppl
+ */
+
+#include "Coordinate.h"
+
+Coordinate::Coordinate() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Coordinate::~Coordinate() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/Coordinate.h
@@ -0,0 +1,17 @@
+/*
+ * Coordinate.h
+ *
+ *  Created on: Apr 2, 2013
+ *      Author: mppl
+ */
+
+#ifndef COORDINATE_H_
+#define COORDINATE_H_
+
+class Coordinate {
+public:
+	Coordinate();
+	virtual ~Coordinate();
+};
+
+#endif /* COORDINATE_H_ */
